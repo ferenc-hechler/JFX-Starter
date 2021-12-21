c1de0a73ae389d277964061fd2873ef50f58d270
feri <github@fh.anderemails.de>
2021-12-21 14:33:52 +0100

    added jar build.

---
+++ src/main/java/de/hechler/experiments/jfxstarter/gui/JFXStarterMain.java
@@ -0,0 +1,10 @@
+package de.hechler.experiments.jfxstarter.gui;
+
+public class JFXStarterMain {
+
+	public static void main(String[] args) {
+		System.out.println(JFXStarterMain.class.getSimpleName()+" started");
+		System.out.println("finished");
+	}
+
+}
