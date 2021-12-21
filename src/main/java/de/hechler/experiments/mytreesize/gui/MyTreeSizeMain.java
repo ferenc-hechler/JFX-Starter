7cbdaf118e26ef79f803a96cea828f92993c7f55
feri <github@fh.anderemails.de>
2021-12-21 14:32:27 +0100

    setup java 17 and junit.

---
+++ src/main/java/de/hechler/experiments/mytreesize/gui/MyTreeSizeMain.java
@@ -0,0 +1,10 @@
+package de.hechler.experiments.mytreesize.gui;
+
+public class MyTreeSizeMain {
+
+	public static void main(String[] args) {
+		System.out.println(MyTreeSizeMain.class.getSimpleName()+" started");
+		System.out.println("finished");
+	}
+
+}
