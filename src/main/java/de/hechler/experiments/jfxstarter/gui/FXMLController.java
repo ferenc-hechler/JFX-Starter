66ba42d500d525653d6bcfade25923ee9e9d7700
feri <github@fh.anderemails.de>
2021-12-22 09:22:51 +0100

    use treeviewtable.

--- src/main/java/de/hechler/experiments/jfxstarter/gui/FXMLController.java
+++ src/main/java/de/hechler/experiments/jfxstarter/gui/FXMLController.java
@@ -1,25 +1,54 @@
 package de.hechler.experiments.jfxstarter.gui;
 
+import java.util.Date;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 
 public class FXMLController {
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private Button button1;
-    
-    
-    public void onButton1Click() {
-        label.setText("Geclickt!");
-    }
+    private TreeTableView<ViewData> ttv;
     
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-    }    
+        TreeTableColumn<ViewData, String> treeTableColumn1 = new TreeTableColumn<>("Name");
+        TreeTableColumn<ViewData, String> treeTableColumn2 = new TreeTableColumn<>("Filesize");
+
+        treeTableColumn1.setCellValueFactory(new TreeItemPropertyValueFactory<>("name"));
+        treeTableColumn2.setCellValueFactory(new TreeItemPropertyValueFactory<>("size"));
+
+        ttv.getColumns().add(treeTableColumn1);
+        ttv.getColumns().add(treeTableColumn2);    
+        
+        TreeItem mercedes1 = new TreeItem(new ViewData("Mercedes SL500", 500L, now()));
+        TreeItem mercedes2 = new TreeItem(new ViewData("Mercedes SL500 AMG", 500L, now()));
+        TreeItem mercedes3 = new TreeItem(new ViewData("Mercedes CLA 200", 200L, now()));
+
+        TreeItem mercedes = new TreeItem(new ViewData("Mercedes ...", 0L, null));
+        mercedes.getChildren().add(mercedes1);
+        mercedes.getChildren().add(mercedes2);
+
+        TreeItem audi1 = new TreeItem(new ViewData("Audi A1", 1L, now()));
+        TreeItem audi2 = new TreeItem(new ViewData("Audi A5", 5L, now()));
+        TreeItem audi3 = new TreeItem(new ViewData("Audi A7", 7L, now()));
+
+        TreeItem audi = new TreeItem(new ViewData("Audi ...", 0L, null));
+        audi.getChildren().add(audi1);
+        audi.getChildren().add(audi2);
+        audi.getChildren().add(audi3);
+
+        TreeItem cars = new TreeItem(new ViewData("Cars ...", 0L, null));
+        cars.getChildren().add(audi);
+        cars.getChildren().add(mercedes);
+
+        ttv.setRoot(cars);        
+        
+    }
+
+	private Date now() {
+		return new Date();
+	}    
 }
---
+++ src/main/java/de/hechler/experiments/jfxstarter/gui/ViewData.java
@@ -0,0 +1,48 @@
+package de.hechler.experiments.jfxstarter.gui;
+
+import java.util.Date;
+
+public class ViewData {
+	
+	private String name;
+	private long size;
+	private Date timestamp;
+	
+
+	public ViewData(String name, long size, Date timestamp) {
+		this.name = name;
+		this.size = size;
+		this.timestamp = timestamp;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getDetails() {
+		return toString();
+	}
+
+	@Override
+	public String toString() {
+		return "VD[" + name + "|" + size + (timestamp==null?"":":"+timestamp) + "]";
+	}
+	
+	
+	
+}
